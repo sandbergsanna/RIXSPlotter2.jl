b99db6e9b4fe64ddfe8d1c06f5e2a96c93c6d61f
sandbergsanna <sandbergsanna@gmail.com>
2025-07-09 13:49:33 +0200

    added eigensystem_plotting

--- src/RIXSPlotter2.jl
+++ src/RIXSPlotter2.jl
@@ -1,8 +1,20 @@
+################################################################################
+#
+#   MODULE FOR PLOTTING RIXS SPECTRA
+#   AUTHOR Jan Attig
+#          Anna Sandberg (2.0)
+#   JULIA v.1+
+#
+################################################################################
+
 module RIXSPlotter2
 
-# using
+    # using
     using PyPlot
     using LinearAlgebra
     using RIXSCalculator2
+
+    # include files
+    include("eigensystem_plotting.jl")
 
 end
