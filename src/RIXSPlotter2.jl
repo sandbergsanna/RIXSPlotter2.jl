a3f636863bb560cabc49878d98835c9d50ab264a
sandbergsanna <sandbergsanna@gmail.com>
2025-07-09 15:09:32 +0200

    added spectrum_plotting.jl

--- src/RIXSPlotter2.jl
+++ src/RIXSPlotter2.jl
@@ -17,5 +17,6 @@ module RIXSPlotter2
     # include files
     include("eigensystem_plotting.jl")
     include("lab_system_plotting.jl")
+    include("spectrum_plotting.jl")
 
 end
---
+++ src/spectrum_plotting.jl
@@ -0,0 +1,67 @@
+################################################################################
+#
+#   Spectrum plotting functions
+#
+################################################################################
+
+
+# plot a spectrum that has been calculated before
+function plot_spectrum(
+        spectrum    :: S,
+        energies    :: Vector{<:Real};
+        new_figure  :: Bool = true,
+        show_figure :: Bool = true,
+        plot_label  :: String = "",
+        plot_color  :: Any = "b",
+        plot_transitions :: Bool = false
+    ) where {T,S<:AbstractSpectrum{T}}
+
+    # configure the plot
+    if new_figure
+        figure()
+        xlabel("energy [meV]")
+        ylabel("intensity [a.u.]")
+    end
+
+    # plot the spectrum
+    plot(
+        energies, [intensity(spectrum, omega) for omega in energies],
+        label=plot_label, color=plot_color
+    )
+
+    # add a legend
+    if plot_label != ""
+        legend()
+    end
+
+    # maybe plot transitions
+    if plot_transitions
+        transitions = spectrum.transitions
+        intensities = weight.(transitions)
+        intensities ./= sum(intensities)
+        intensities ./= maximum(intensities)
+        for t in 1:length(transitions)
+            if intensities[t] > 0.01
+                axvline(frequency(transitions[t]), color="k", alpha=intensities[t])
+            end
+        end
+    end
+
+    # set the x limits
+    xlim(energies[1], energies[end])
+    ylim(0, ylim()[2])
+
+    # tighten the layout
+    tight_layout()
+
+    # show the plot
+    if show_figure
+        show()
+    end
+end
+
+
+
+
+# export functions
+export plot_spectrum
